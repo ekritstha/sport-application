7a4f8894f66b14c8d7a97f9f5320925897439c68
Akrit Shrestha <akrit.act360@gmail.com>
2020-08-04 22:54:42 +0545

    dropdown for sports in events page added

--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useMemo } from "react";
 import api from "../../services/api";
 import {
+  Alert,
   Container,
   Button,
   Form,
   FormGroup,
   Input,
   Label,
-  Alert,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  ButtonDropdown,
 } from "reactstrap";
 import cameraIcon from "../../assets/camera.png";
 import "./events.css";
@@ -17,16 +21,17 @@ export default function EventsPage({ history }) {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
-  const [sport, setSport] = useState("");
+  const [sport, setSport] = useState("Sport");
   const [date, setDate] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [dropdownOpen, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!dropdownOpen);
 
   const preview = useMemo(() => {
     return thumbnail ? URL.createObjectURL(thumbnail) : null;
   }, [thumbnail]);
-
-  console.log(title, description, price, sport);
 
   const submitHandler = async (evt) => {
     evt.preventDefault();
@@ -46,7 +51,7 @@ export default function EventsPage({ history }) {
         title !== "" &&
         description !== "" &&
         price !== "" &&
-        sport !== "" &&
+        sport !== "Sport" &&
         date !== "" &&
         thumbnail !== null
       ) {
@@ -67,96 +72,104 @@ export default function EventsPage({ history }) {
     }
   };
 
+  const sportEventHandler = (sport) => setSport(sport);
+
+  console.log(sport);
   return (
     <Container>
       <h2>Create your Event</h2>
       <Form onSubmit={submitHandler}>
-        <FormGroup>
-          <Label>Upload Image: </Label>
-          <Label
-            id="thumbnail"
-            style={{ backgroundImage: `url(${preview})` }}
-            className={thumbnail ? "has-thumbnail" : ""}
-          >
+        <div className="input-group">
+          <FormGroup>
+            <Label>Upload Image: </Label>
+            <Label
+              id="thumbnail"
+              style={{ backgroundImage: `url(${preview})` }}
+              className={thumbnail ? "has-thumbnail" : ""}
+            >
+              <Input
+                type="file"
+                onChange={(evt) => setThumbnail(evt.target.files[0])}
+              />
+              <img
+                src={cameraIcon}
+                style={{ maxWidth: "50px" }}
+                alt="upload icon image"
+              />
+            </Label>
+          </FormGroup>
+          <FormGroup>
+            <Label>Title: </Label>
             <Input
-              type="file"
-              onChange={(evt) => setThumbnail(evt.target.files[0])}
+              id="title"
+              type="text"
+              value={title}
+              placeholder={"Event Title"}
+              onChange={(evt) => setTitle(evt.target.value)}
             />
-            <img
-              src={cameraIcon}
-              style={{ maxWidth: "50px" }}
-              alt="upload icon"
+          </FormGroup>
+          <FormGroup>
+            <Label>Event description: </Label>
+            <Input
+              id="description"
+              type="text"
+              value={description}
+              placeholder={"Event Description"}
+              onChange={(evt) => setDescription(evt.target.value)}
             />
-          </Label>
+          </FormGroup>
+          <FormGroup>
+            <Label>Event price: </Label>
+            <Input
+              id="price"
+              type="text"
+              value={price}
+              placeholder={"Event Price £0.00"}
+              onChange={(evt) => setPrice(evt.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Event date: </Label>
+            <Input
+              id="date"
+              type="date"
+              value={date}
+              placeholder={"Event Price £0.00"}
+              onChange={(evt) => setDate(evt.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+              <Button id="caret" value={sport} disabled>
+                {sport}
+              </Button>
+              <DropdownToggle caret />
+              <DropdownMenu>
+                <DropdownItem onClick={() => sportEventHandler("running")}>
+                  running
+                </DropdownItem>
+                <DropdownItem onClick={() => sportEventHandler("cycling")}>
+                  cycling
+                </DropdownItem>
+                <DropdownItem onClick={() => sportEventHandler("swimming")}>
+                  swimming
+                </DropdownItem>
+              </DropdownMenu>
+            </ButtonDropdown>
+          </FormGroup>
+        </div>
+        <FormGroup>
+          <Button className="submit-btn">Submit</Button>
         </FormGroup>
         <FormGroup>
-          <Label>Sport: </Label>
-          <Input
-            id="sport"
-            type="text"
-            value={sport}
-            placeholder={"Sport name"}
-            onChange={(evt) => setSport(evt.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Title: </Label>
-          <Input
-            id="title"
-            type="text"
-            value={title}
-            placeholder={"Event Title"}
-            onChange={(evt) => setTitle(evt.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Event description: </Label>
-          <Input
-            id="description"
-            type="text"
-            value={description}
-            placeholder={"Event Description"}
-            onChange={(evt) => setDescription(evt.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Event price: </Label>
-          <Input
-            id="price"
-            type="text"
-            value={price}
-            placeholder={"Event Price 0.00$"}
-            onChange={(evt) => setPrice(evt.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Event date: </Label>
-          <Input
-            id="date"
-            type="date"
-            value={date}
-            placeholder={"Event Price £0.00"}
-            onChange={(evt) => setDate(evt.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Button type="submit" className="submit-btn">
-            Create Event
-          </Button>
-        </FormGroup>
-        <FormGroup>
-          <Button
-            type="submit"
-            className="secondary-btn"
-            onClick={() => history.push("/")}
-          >
+          <Button className="secondary-btn" onClick={() => history.push("/")}>
             Dashboard
           </Button>
         </FormGroup>
       </Form>
       {error ? (
         <Alert className="event-validation" color="danger">
-          {""}
+          {" "}
           Missing required information
         </Alert>
       ) : (
