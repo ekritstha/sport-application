cd2ca45cc3ec248d2116306eba6dbb2ed37191a7
Akrit Shrestha <akrit.act360@gmail.com>
2020-08-04 22:11:11 +0545

    events added to dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import moment from "moment";
 
 //will show all the events
 export default function Dashboard() {
-  return <div>hello from Dashboard</div>;
+  const [events, setEvents] = useState([]);
+  const user_id = localStorage.getItem("user");
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const getEvents = async (filter) => {
+    const url = filter ? `/dashboard/${filter}` : `/dashboard`;
+    const response = await api.get(url, { headers: { user_id } });
+
+    setEvents(response.data);
+  };
+  // console.log(events);
+  return (
+    <ul className="events-list">
+      {events.map((event) => (
+        <li key={event._id}>
+          <header style={{ backgroundImage: `url(${event.thumbnail_url})` }} />
+          <strong>{event.title}</strong>
+          <span>{moment(event.date).format("MMM Do YY")}</span>
+          <span>{event.price}</span>
+          <span>{event.description}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
